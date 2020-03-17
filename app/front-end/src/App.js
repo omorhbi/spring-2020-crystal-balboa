9c05a59e67edb2b1567bb6b60adb62f2f00ccf11
EdwardCheung99 <35943871+EdwardCheung99@users.noreply.github.com>
2020-03-17 18:04:34 -0400

    Set up react app, begin work on welcome page

--- app/front-end/src/App.js
+++ app/front-end/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import desktopBg from './images/deskWelcome.png';
+import mobileBg from './images/mobileWelcome.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	let image;
+	if(window.innerWidth >= 650){
+		image = desktopBg;
+	}
+	else{
+		image = mobileBg;
+	}
+	return (
+		<div className="welcome" style={{backgroundImage: `url(${image})` }}>
+			<div className="welcome-text">
+				<h1>"What will I eat for dinner?"</h1>
+				<p>Supperwhere is an application to help you figure out dining recommendations based on your dietary preferences and meal history. 
+				Supperwhere aims to make the dining experience more convenient for you by providing quick and easy options that are tailored to your preferences.
+				</p>
+			</div>
+		</div>
+	);
+};
 
 export default App;
