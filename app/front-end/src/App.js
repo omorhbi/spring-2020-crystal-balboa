15f0a7a6c853d6696c99cfaca938f0e25140fe13
EdwardCheung99 <35943871+EdwardCheung99@users.noreply.github.com>
2020-03-17 21:32:43 -0400

    Create welcome page for desktop

--- app/front-end/src/App.js
+++ app/front-end/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import desktopBg from './images/deskWelcome.png';
+import desktopBg from './images/deskWelcome2.png';
 import mobileBg from './images/mobileWelcome.png';
+import logo from './images/supperwhere1.png';
 
 const App = () => {
 	let image;
@@ -14,8 +14,10 @@ const App = () => {
 	}
 	return (
 		<div className="welcome" style={{backgroundImage: `url(${image})` }}>
+			<img src={logo} id="welcome-logo"/>
+			<div id="welcome-login"><button type="button" id="login-button">Login/Register</button></div>
 			<div className="welcome-text">
-				<h1>"What will I eat for dinner?"</h1>
+				<h1>"What should I eat for dinner?"</h1>
 				<p>Supperwhere is an application to help you figure out dining recommendations based on your dietary preferences and meal history. 
 				Supperwhere aims to make the dining experience more convenient for you by providing quick and easy options that are tailored to your preferences.
 				</p>
