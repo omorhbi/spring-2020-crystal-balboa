e00a1cd3f0a98b588663012a6a2d6da185fee3a9
EdwardCheung99 <35943871+EdwardCheung99@users.noreply.github.com>
2020-04-09 16:53:27 -0400

    create route for search

---
+++ app/back-end/app.js
@@ -0,0 +1,52 @@
+// import and instantiate express
+const express = require('express'); // CommonJS import style!
+const app = express(); // instantiate an Express object
+const bodyParser = require('body-parser');
+const zomato = require('zomato.js');
+const apiKey = process.env.ZOMATO_API_KEY; //export ZOMATO_API_KEY="key"
+const zomatoClient = new zomato(apiKey);
+
+// use the bodyparser middleware to parse any data included in a request
+app.use(bodyParser.json());  // decode JSON-formatted incoming POST data
+app.use(bodyParser.urlencoded({extended: true})); // decode url-encoded incoming POST data
+
+// post request to show search results
+app.post('/location/show', (req, res) => {
+	zomatoClient.locations({
+	query: "New York City", //Location
+	count: 1
+	})
+	.then(function(data) {
+		const entity_id = data[0].entity_id;
+		const entity_type = data[0].entity_type;
+		zomatoClient.search({
+			entity_id: entity_id,
+			entity_type: entity_type,
+			count: 10,
+			q: "American" //Search query
+		})
+		.then(function(resData){
+			//console.log(resData)
+			const restaurants = resData.restaurants.map(r => {
+				return {
+					name: r.name,
+					location: r.location.address,
+					price: r.price_range,
+					thumbnail: r.thumb,
+					rating: r.user_rating.aggregate_rating,
+					cuisine: r.cuisines
+		      	}
+		    })
+		    res.json({ restaurants });
+		})
+		.catch(function(err){
+			console.log(err)
+		});
+    })
+    .catch(function(err) {
+    	console.error(err);
+    });
+});
+
+// export the express app we created to make it available to other modules
+module.exports = app;
