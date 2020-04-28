c2704be56a754fb4eb64b9f6da64775d34315437
BryanChan0220 <55110589+BryanChan0220@users.noreply.github.com>
2020-04-27 22:46:46 -0400

    Added filtering to Meal History

--- app/front-end/src/meal_history.js
+++ app/front-end/src/meal_history.js
@@ -13,7 +13,9 @@ const Meal_History = (props) =>{
         history.push('/login');
     }
   
+    const [sampleFilter, setFilter] = useState('all');
     const [sampleRestaurants, setSampleRestaurants] = useState([]);
+    const [sampleRestaurants2, setSampleRestaurants2] = useState([]);
 
     useEffect (() =>{
         axios.get('./meal_history')
@@ -32,6 +34,7 @@ const Meal_History = (props) =>{
             })
             console.log(resRests);
             setSampleRestaurants(resRests);
+            setSampleRestaurants2(resRests);
             console.log(sampleRestaurants);
         })
     }, []);
@@ -49,6 +52,53 @@ const Meal_History = (props) =>{
             })
     };
 
+    const handleChange = (event) => {
+        setFilter(event.target.value);
+    }
+
+    const handleFilter = (event) => {
+        let d = new Date();
+        d = d.toLocaleDateString();
+        console.log(d);
+        const dateData = d.slice('/');
+        let results = [];
+
+        switch (sampleFilter) {
+            case "all" : 
+                setSampleRestaurants(sampleRestaurants2);
+                break;
+            case "year" :
+                for(let i = 0; i < sampleRestaurants2.length; i++){
+                    if(sampleRestaurants2[i].date.slice('/')[2] === dateData[2]){
+                        results.push(sampleRestaurants2[i]);
+                    }
+                }
+                setSampleRestaurants(results);
+                break;
+            case "month":
+                for(let i = 0; i < sampleRestaurants2.length; i++){
+                    if(sampleRestaurants2[i].date.slice('/')[2] === dateData[2]){
+                        if(sampleRestaurants2[i].date.slice('/')[0] === dateData[0]){
+                            results.push(sampleRestaurants2[i]);
+                        }
+                    }
+                }
+                setSampleRestaurants(results);
+                break;
+            case "day":
+                for(let i = 0; i < sampleRestaurants2.length; i++){
+                    if(d === sampleRestaurants2[i].date){
+                        results.push(sampleRestaurants2[i]);
+                    }
+                }
+                setSampleRestaurants(results);
+                break;
+        }
+
+
+    }
+
+
         return(
             <div id="parent">
             <h1 className="meal_h">Meal History</h1>
@@ -56,14 +106,13 @@ const Meal_History = (props) =>{
             
             <div id="meal_table">
                 <label for="time_frames" id="time_f"> View entries from: </label>
-                <select id="time_frames" name="time_frames">
+                <select id="time_frames" name="time_frames" onChange={handleChange}>
                     <option value="all">All Time</option>
                     <option value="year">This Year</option>
-                    <option value="month">Last Month</option>
-                    <option value="week">Last Week</option>
+                    <option value="month">This Month</option>
                     <option value="day">Today</option>
                     </select>
-                    <input className="option_button" type="submit"></input>
+                    <input className="option_button" type="submit" onClick={handleFilter}></input>
             </div>
             
 
