59e6cd6c795f47bc5c7c766c12abdbd6445d38c9
Omar Morhbi <omorhbi@gmail.com>
2020-08-28 20:55:56 -0400

    added build folder for heroku

--- app.js
+++ app.js
@@ -395,14 +395,13 @@ app.post('/preferences', authorized(), (req,res) => {
 
 });
 
-if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'front-end')));
-// Handle React routing, return all requests to React app
-  app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'front-end', 'index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+ 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 
 // export the express app we created to make it available to other modules
---
+++ build/precache-manifest.fcea1b15bbdd60297e2483c352c1eb08.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be779d7fe164c3c5f66f528ba12956f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e0da086e5ea8e07867a",
+    "url": "/static/css/main.b8e13af6.chunk.css"
+  },
+  {
+    "revision": "42701380a08980490875",
+    "url": "/static/js/2.f59060e0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f59060e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0da086e5ea8e07867a",
+    "url": "/static/js/main.ffca8bd2.chunk.js"
+  },
+  {
+    "revision": "f26ba8da7c41e270b6e8",
+    "url": "/static/js/runtime-main.c4f1d106.js"
+  },
+  {
+    "revision": "48d05a583ce87a6c75e1811b3f01da91",
+    "url": "/static/media/deskWelcome2.48d05a58.png"
+  },
+  {
+    "revision": "58ebcc811af8dcaf329f5255119e1fbc",
+    "url": "/static/media/fries.58ebcc81.png"
+  },
+  {
+    "revision": "136cb055ca9060009a13d0a5ff3ee34c",
+    "url": "/static/media/supperwhere1.136cb055.png"
+  },
+  {
+    "revision": "e4f3d13407a194dc32c09d4546468804",
+    "url": "/static/media/supperwhereS1.e4f3d134.png"
+  },
+  {
+    "revision": "0653ae66d92f579bacbc289701aa6e75",
+    "url": "/static/media/supperwhere_login.0653ae66.png"
+  },
+  {
+    "revision": "a2a295a669f78aace0b05a21940d859c",
+    "url": "/static/media/sushi.a2a295a6.png"
+  }
+]);
